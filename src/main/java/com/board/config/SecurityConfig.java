15235b9caa822dfdd175a89bd56c919cbf9e9fa1
thdus12 <bthdus5890@gmail.com>
2023-05-15 01:06:50 +0900

    refactor. jwt 토큰 인증을 이용한 로그인 처리

--- src/main/java/com/board/config/SecurityConfig.java
+++ src/main/java/com/board/config/SecurityConfig.java
@@ -8,9 +8,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.board.handler.AuthFailureHandler;
 import com.board.handler.AuthSucessHandler;
+import com.board.jwt.JwtAuthenticationFilter;
+import com.board.jwt.JwtTokenProvider;
 import com.board.service.member.MemberDetailServiceImpl;
 
 import lombok.RequiredArgsConstructor;
@@ -20,9 +23,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	private final JwtTokenProvider jwtTokenProvider;
 	private final MemberDetailServiceImpl memberDetailServiceImpl;
 	private final AuthSucessHandler authSucessHandler;
 	private final AuthFailureHandler authFailureHandler;
+	
+	// JWT 인증 필터 빈을 생성하는 메서드
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+	    // JwtAuthenticationFilter 객체 생성 및 초기화
+		JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager(), jwtTokenProvider, memberDetailServiceImpl);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login/**");
+        return jwtAuthenticationFilter;
+    }
 	
 	// BCryptPasswordEncoder는 Spring Security에서 제공하는 비밀번호 암호화 객체 (BCrypt라는 해시 함수를 이용하여 패스워드를 암호화 한다.)
 	// 회원 비밀번호 등록시 해당 메서드를 이용하여 암호화해야 로그인 처리시 동일한 해시로 비교한다.
@@ -37,34 +50,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	    auth.userDetailsService(memberDetailServiceImpl).passwordEncoder(encryptPassword());
 	}
 	
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	http.csrf().disable() 	 // csrf 토큰을 비활성화
-	        .authorizeRequests() // 요청 URL에 따라 접근 권한을 설정
-	        .antMatchers("/admin/**").hasRole("ADMIN")
-	        .antMatchers("/", "/login/**", "/js/**", "/css/**", "/image/**").permitAll() // 해당 경로들은 접근을 허용
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.csrf().disable() 	 // CSRF 토큰 비활성화
+	        .authorizeRequests() // 요청 URL에 따라 접근 권한 설정
+	            .antMatchers("/admin/**").hasRole("ADMIN")
+	            .antMatchers("/", "/login/**", "/js/**", "/css/**", "/image/**").permitAll() // 해당 경로들은 접근 허용
+	            .anyRequest().authenticated()
 	        .and()
-	        .formLogin() 		 // 로그인 폼
-	        .loginPage("/login") // 해당 주소로 로그인 페이지를 호출한다.
-	        .loginProcessingUrl("/login/action") // 해당 URL로 요청이 오면 스프링 시큐리티가 가로채서 로그인처리를 한다. -> loadUserByName
-	        .successHandler(authSucessHandler) 	 // 성공시 요청을 처리할 핸들러
-	        .failureHandler(authFailureHandler)  // 실패시 요청을 처리할 핸들러
+	        .formLogin() 			 // 로그인 설정
+	            .loginPage("/login") // 로그인 페이지 경로
+	            .loginProcessingUrl("/login/action") // 로그인 액션 처리 URL
+	            .successHandler(authSucessHandler)   // 로그인 성공 핸들러 jwt 토큰 생성
+	            .failureHandler(authFailureHandler)  // 로그인 실패 핸들러
 	        .and()
-	        .logout()
-	        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 URL
-	        .logoutSuccessUrl("/login")  // 성공시 리턴 URL
-	        .invalidateHttpSession(true) // 인증정보를 지우하고 세션을 무효화
-	        .deleteCookies("JSESSIONID","remember-me") // JSESSIONID, remember-me 쿠키 삭제
-	        .permitAll()
+	        .logout() // 로그아웃 설정
+	            .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 URL
+	            .logoutSuccessUrl("/login")  // 로그아웃 성공시 반환 URL
+	            .invalidateHttpSession(true) // 세션 무효화
+	            .deleteCookies("JSESSIONID", "remember-me") // 쿠키 삭제
+	            .permitAll()
 	        .and()
-	        .sessionManagement()
-	        .maximumSessions(1) 			 // 세션 최대 허용 수 1, -1인 경우 무제한 세션 허용
-	        .maxSessionsPreventsLogin(false) // true면 중복 로그인을 막고, false면 이전 로그인의 세션을 해제
-	        .expiredUrl("/login?error=true&exception=Have been attempted to login from a new place. or session expired") // 세션이 만료된 경우 이동 할 페이지를 지정
+	        .sessionManagement() 	// 세션 관리 설정
+	            .maximumSessions(1) // 최대 허용 세션 수 (1)
+	            .maxSessionsPreventsLogin(false) // 동시 로그인 방지 기능 비활성화
+	            .expiredUrl("/login?error=true&exception=Have been attempted to login from a new place. or session expired") // 세션이 만료된 경우 이동할 URL
 	        .and()
-	        .and().rememberMe()    		 // 로그인 유지
-	        .alwaysRemember(false) 		 // 항상 기억할 것인지 여부
-	        .tokenValiditySeconds(43200) // in seconds, 12시간 유지
-	        .rememberMeParameter("remember-me");
-    }
+	        .and().rememberMe() 			 // 로그인 유지 설정
+	            .alwaysRemember(false) 		 // 항상 기억할지 여부
+	            .tokenValiditySeconds(43200) // 토큰 유효 시간 (43200초, 12시간)
+	            .rememberMeParameter("remember-me")
+	        .and()
+	        // 첫 번째 인자로 추가할 필터 객체를 지정하고, 
+	        // 두 번째 인자로 해당 필터보다 앞에 위치할 필터의 클래스를 지정함
+	        // 필터 체인에서 JwtAuthenticationFilter는 
+	        // 다른 인증과 관련된 필터보다 먼저 호출되어 JWT 토큰을 검증하고 사용자를 인증함
+	        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+
 }
--- src/main/java/com/board/handler/AuthSucessHandler.java
+++ src/main/java/com/board/handler/AuthSucessHandler.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import com.board.entity.member.MemberRepository;
+import com.board.jwt.JwtTokenProvider;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -16,11 +18,17 @@ import lombok.RequiredArgsConstructor;
 public class AuthSucessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
         memberRepository.updateMemberLastLogin(authentication.getName(), LocalDateTime.now());
+
+        // JWT 토큰 생성
+        String token = jwtTokenProvider.generateToken(authentication.getName());
+
+        response.addHeader("Authorization", "Bearer " + token);
+
         setDefaultTargetUrl("/board/list");
 
         super.onAuthenticationSuccess(request, response, authentication);
